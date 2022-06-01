0812f07d57b2d9fe52fe5508ad1c2b4a4d632c09
Andres Baeza <andresftwin@gmail.com>
2022-06-01 16:47:41 -0700

    connect posts to own page and URL, othe minor changes

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const homeContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+
+const aboutContent = "I live in Portland, Oregon but originally from Hood River. It's a small city located in the beautiful Colombia River Gorge about an hour from Portland. Being from a small city definitely had it's benefits. My friends lived nearby, school was always a few minutes away, and the community was always kind and welcoming. Early on I developed a passion for soccer and continued with it until the end of my college years. I played Division 2 and loved every moment of it. After I graduated with a bachelors in exercise and sports science with an empahsis in physical therapy, in 2020, I continued playing soccer in local leagues and pick up games throughout the Portland area. Although I was still involved with soccer, it didn't take nearly as much time as it did before which meant I had tons of spare time. During this time was when I wrote my first 'Hello World!' - it was exhilarating.";
+
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
@@ -19,6 +21,9 @@ app.use(express.static("public"));
 
 //empty array which title and content get pushed to and saved
 let posts = [];
+
+let title = [];
+let content = [];
 
 app.get('/', function(req, res) {
   res.render('home', {
@@ -72,15 +77,17 @@ app.get('/post/:postName', function(req, res) {
 
     const storedTitle = post.title.toLowerCase().replaceAll('-',' ');
 
+    //renders new page with post title and content matching post/:postName
     if (storedTitle === requestedTitle) {
-      console.log(storedTitle);
-      console.log('It matches!');
-    } else {
-      console.log('No match!');
+      res.render('post', {
+        title: post.title,
+        content: post.content
+      })
     }
   });
 
 });
+
 
 
 
